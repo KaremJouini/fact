79c5265826337a83ea27900fb89a49b45fb0942c
eVoo <superyassine15@gmail.com>
2020-11-06 00:53:07 +0100

    Initial commit

---
+++ fact.test.js
@@ -0,0 +1,11 @@
+
+const fact = require("./fact");
+test('fact 5 ', () => {
+    expect(fact(5)).toBe(120);
+});
+test('fact 0 ', () => {
+    expect(fact(1)).toBe(1);
+});
+test('fact -1 ', () => {
+    expect(fact(-1)).toBe(0);
+});
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const fact = require("./fact");
+const app = express()
+const port = 3000
+
+app.get('/fact', (req, res) => {
+    const n = req.query.n;
+    res.send(fact(n).toString())
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+
